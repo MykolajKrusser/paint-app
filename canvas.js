fcd026f49cf846f3c63fe397d35d26570245f8d5
Mykolaj <32041427+MykolajKrusser@users.noreply.github.com>
2018-02-03 13:09:02 +0100

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -1,32 +1,32 @@
-let canvas =  document.getElementById("test");
-let ctx = canvas.getContext("2d");
-let myColor = "black";
-let rSize = 5;
-document.getElementById("color").oninput = function(){
+let canvas =  document.getElementById("test");// declare variables of element
+let ctx = canvas.getContext("2d"); //context
+let myColor = "black"; //start color of brush
+let rSize = 5; //start size of brush
+document.getElementById("color").oninput = function(){ //function for change brush color
     myColor = this.value;
     ctx.beginPath();
 };
-document.getElementById('size').oninput = function(){
+document.getElementById('size').oninput = function(){ //function for change brush size
     rSize = this.value;
 };
-canvas.onmousedown = function (event) {
-    let x = event.offsetX;
+canvas.onmousedown = function (event) {//main paint function fire when mouse button active
+    let x = event.offsetX; // read mouse coordinates on event
     let y = event.offsetY;
-    ctx.strokeStyle = myColor;
-    ctx.lineCap = "round";
-    ctx.moveTo (x,y);
-    canvas.onmousemove = function (event) {
+    ctx.strokeStyle = myColor; // color of the built line
+    ctx.lineCap = "round"; // rounding of elements
+    ctx.moveTo (x,y); //start point paint line
+    canvas.onmousemove = function (event) { //on move mouse function for create solid line
         let x = event.offsetX;
         let y = event.offsetY;
-        ctx.lineWidth = rSize;
-        ctx.lineTo (x,y);
-        ctx.stroke();
+        ctx.lineWidth = rSize; // changeable line width
+        ctx.lineTo (x,y); //end coordinates of line
+        ctx.stroke(); // command to "draw" everything that happened here))
     };
-    canvas.onmouseup = function () {
+    canvas.onmouseup = function () { //when mouse button not active we stop draw
         canvas.onmousemove = null;
     }
 };
-document.getElementById('pClear').onclick = function(){
+document.getElementById('pClear').onclick = function(){ //function for clear canvas element
     ctx.beginPath();
     ctx.clearRect(0, 0, 500, 500);
 };
--- preloader.js
+++ preloader.js
@@ -1,18 +1,18 @@
-let getPreLoader = document.getElementById("preLoader"); // get an element
+let getPreLoader = document.getElementById("preLoader"); // access to the element
 // changing visibility by decrementing opacity from 1 to 0 and change style element to display "none".
 function fadeOut(el){
-    el.style.opacity = 1;
-    let decrementOpacity = setInterval(function(){
+    el.style.opacity = 1; //element opacity
+    let decrementOpacity = setInterval(function(){ //function for decrement element opacity from 1 to 0 and display "none"
         el.style.opacity = el.style.opacity - 0.05;
         if (el.style.opacity <=0.05){
             clearInterval(decrementOpacity);
-            getPreLoader.style.display = "none";
+            getPreLoader.style.display = "none"; //if opacity <=0.05 we stop decrement and hide element
         }
-    }, 20);
+    }, 20); //every 20 milliseconds decrement opacity
 }
 // calling the function fadeOut.
-window.onload = function loadStart(){
+window.onload = function loadStart(){ //use event of window loading for fire function FadeOut, which change opacity and hide pre-loader
     setTimeout(function(){
         fadeOut(getPreLoader);
-    },1000);
+    },1000); // 1 sec
 };
